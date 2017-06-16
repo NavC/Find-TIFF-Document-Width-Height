ad50c6f543404833370228cf9cc8f362a414f8f0
Nav <navis@DESKTOP-Q4F6ITI>
2017-06-16 09:55:57 -0700

    GUI Setup

--- Find-TIFF-Document-Width-Height/Form1.Designer.cs
+++ Find-TIFF-Document-Width-Height/Form1.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnFindDimensions = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnFindDimensions
+            // 
+            this.btnFindDimensions.Location = new System.Drawing.Point(73, 192);
+            this.btnFindDimensions.Name = "btnFindDimensions";
+            this.btnFindDimensions.Size = new System.Drawing.Size(132, 46);
+            this.btnFindDimensions.TabIndex = 0;
+            this.btnFindDimensions.Text = "Find Dimensions";
+            this.btnFindDimensions.UseVisualStyleBackColor = true;
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(12, 95);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(156, 31);
+            this.lblSize.TabIndex = 1;
+            this.lblSize.Text = "Dimensions";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 250);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.btnFindDimensions);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Find TiFF Dimensions";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFindDimensions;
+        private System.Windows.Forms.Label lblSize;
     }
 }
 
