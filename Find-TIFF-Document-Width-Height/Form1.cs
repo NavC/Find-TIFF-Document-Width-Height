dddff7d8b23b70ea4afe183e193d29d72e1d8e43
navis <navis@DESKTOP-Q4F6ITI>
2017-06-16 09:50:17 -0700

    Add project files.

---
+++ Find-TIFF-Document-Width-Height/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Find_TIFF_Document_Width_Height
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
